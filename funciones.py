608040d6bf77c11c29ac6a5df9947138f53e7c95
hideo <hideo03@gmail.com>
2014-10-01 23:06:29 +0200

    python funciones

---
+++ funciones.py
@@ -0,0 +1,10 @@
+__author__ = 'david'
+def cube(number):
+    number = number * number * number
+    return number
+
+def by_three(number):
+    if number % 3 == 0:
+        return cube(number)
+    else:
+        return False
