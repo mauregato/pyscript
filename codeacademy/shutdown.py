6f901dda0083822cf38797f5d33db0a8a041fe9f
hideo <hideo03@gmail.com>
2014-10-01 23:34:48 +0200

    no message

---
+++ codeacademy/shutdown.py
@@ -0,0 +1,8 @@
+__author__ = 'david'
+def shut_down(s):
+    if shut_down(s) == yes:
+        return "Shutting down"
+    elif shut_down(s) == no:
+        return "Suttdonw aborted"
+    else:
+        return "Sorry"
