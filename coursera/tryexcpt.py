30bc610eb99d212aa791f7dab1f25237396dd3fc
David Pastur <david@MacBook-de-David-Pastur.local>
2015-02-24 00:02:26 +0100

    add tryexcept.py

---
+++ coursera/tryexcpt.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+astr= 'Hello Bob'
+try:
+    istr = int(astr)
+except:
+    istr = -1
+
+print 'First',istr
+
+astr = '123'
+try:
+    istr = int(astr)
+except:
+    istr = -1
+
+print 'Second',istr 
