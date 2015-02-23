5608fc455f1b574fdf2574cffb140435cf523d5b
David Pastur <david@MacBook-de-David-Pastur.local>
2015-02-24 00:09:26 +0100

    add trynumber.py

---
+++ coursera/trynum.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+rawstr = raw_input('Enter a number:')
+
+try:
+    ival = int(rawstr)
+except:
+    ival = -1
+
+if ival > 0:
+    print 'Nice work'
+else:
+    print 'Not a number'
