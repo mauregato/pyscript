17a0debb4ea2c8d67443dc09af73577bf4db6d49
mauregato <hideo03@gmail.com>
2019-05-21 11:00:00 +0200

    .

---
+++ crash_course/ch3/3-2greetings.py
@@ -0,0 +1,7 @@
+names=['Jordan', 'Victor', 'Alex', 'Pedro']
+greetings = "Good morning "
+
+print(greetings + names[0])
+print(greetings + names[1])
+print(greetings + names[2])
+print(greetings + names[-1])
---
+++ crash_course/ch3/3-3ownlist.py
@@ -0,0 +1,4 @@
+transport = ['black', 'Suzuki', 'small', 'economic']
+print('I would like to buy a ' + transport[1] + ' car ')
+print('I prefer a ' + transport[2] + ' car ')
+print('A ' + transport[0] + ' colour is more ' + transport[3])
