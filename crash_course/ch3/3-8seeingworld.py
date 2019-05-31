b2ac5f395902db241440be8a47a5cea76c98ba72
mauregato <hideo03@gmail.com>
2019-05-31 11:25:36 +0200

    finish chapter3

--- crash_course/ch3/3-8seeingworld.py
+++ crash_course/ch3/3-8seeingworld.py
@@ -1,4 +1,13 @@
 location = [ 'roma', 'tokyo', 'moscow', 'london', 'sidney' ]
 print(location)
+#order alphabetically
 print(sorted(location))
+print(location)
+location.reverse()
+print(location)
+location.reverse()
+print(location)
+location.sort()
+print(location)
+location.sort(reverse=True)
 print(location)
